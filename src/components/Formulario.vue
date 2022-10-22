f2eb414b9d4148379a0cff4f1dcc570d7e49b3bf
Jacks Bruno Moran Prado <jacks.prado@grupopereira.local>
2022-10-22 09:10:02 -0400

    modificando o estado

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -2,7 +2,7 @@
   <div class="box formulario">
     <div class="columns">
       <div
-        class="column is-8"
+        class="column is-5"
         role="form"
         aria-label="Formulário para criação de uma nova tarefa"
       >
@@ -13,6 +13,21 @@
           v-model="descricao"
         />
       </div>
+      <div
+        class="column is-3">
+        <div class="select">
+          <select v-model="idProjeto">
+            <option value="">Selecione o projeto</option>
+            <option
+              v-for="list in meusProjetos"
+              :value="list.id"
+              :key="list.id"
+            >
+              {{ list.nome }}
+            </option>
+          </select>
+        </div>
+      </div>
       <div class="column">
         <Temporizador @aoTemporizadorFinalizado="finalizarTarefa"/>
       </div>
@@ -21,7 +36,9 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { computed, defineComponent } from "vue";
+import { useStore } from "vuex";
+import { key } from '@/store'
 import Temporizador from './Temporizador.vue'
 
 export default defineComponent({
@@ -32,16 +49,24 @@ export default defineComponent({
   },
   data () {
     return {
-      descricao: ''
+      descricao: '',
+      idProjeto: ''
     }
   },
   methods: {
     finalizarTarefa (tempoDecorrido: number) : void {
       this.$emit('aoSalvarTarefa', {
         duracaoEmSegundos: tempoDecorrido,
-        descricao: this.descricao
+        descricao: this.descricao,
+        projeto: this.meusProjetos.find(proj => proj.id == this.idProjeto)
       })
       this.descricao = ''
+    }
+  },
+  setup(){
+    const store = useStore(key)
+    return {
+      meusProjetos: computed(() => store.state.projetos)
     }
   }
 });
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import App from './App.vue'
 
 import '@fortawesome/fontawesome-free/css/all.css'
 import roteamento from './router/index'
+import { key, store } from './store'
 
 createApp(App)
 .use(roteamento)
+.use(store, key)
 .mount('#app')
